7fc1432f54aa84dae9575eb9dd2b5507c782e937
lilloberry <99738029+lilloberry@users.noreply.github.com>
2022-03-30 19:22:38 +0400

    Add files via upload

---
+++ src/Components/Description.jsx
@@ -0,0 +1,19 @@
+import './Description.css';
+
+function Description() {
+return (
+    <description className="Description">
+        <img
+            src = 'https://travelask.ru/system/embedded_map_attachments/files/000/001/867/original/%D1%81%D0%B0%D0%BC%D0%B0%D1%80%D1%81%D0%BA%D0%B0%D1%8F_%D0%BB%D1%83%D0%BA%D0%B0.jpg'>
+        </img>
+        <div>
+        <nav>
+        Этот сайт предназначен для просмотра животных, которые живут на территории Самарской области, а также находятся в красной книге Самарской области.
+        Здесь можно также просмотреть заповедные зоны Самарской области.
+        </nav>
+    </div>
+    </description>
+);
+}
+
+export default Description;
