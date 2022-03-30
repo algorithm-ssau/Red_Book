0781de58aeb45d9e36032008a09d3a7b4a632fcd
Marya Malyugina <loveamu@mail.ru>
2022-03-30 17:42:55 +0400

    Menu bar

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,17 @@
+
+function Menu() {
+  return (
+    <div>
+        <nav>
+            <a href='#s'>Млекопитающие</a>-
+            <a href='#s'>Пресмыкающиеся</a>-
+            <a href='#s'>Насекомые</a>-
+            <a href='#s'>Рыбы</a>-
+            <a href='#s'>ОПЗ</a>-
+
+        </nav>
+    </div>
+  );
+}
+
+export default Menu;
