cae0a548918992c5ccc6fc60734d1736f876d312
Nadya Selickaya <nadinehm2210@mail.ru>
2022-04-04 23:28:30 +0400

    EXAMPLE COMMIT NOT SERIOSLY

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ import Menu from './Components/Menu';
 function App() {
   return (
     <div>
+      <header>
+        <img src='https://w7.pngwing.com/pngs/16/66/png-transparent-red-and-white-book-logo-e-book-just-listen-discover-the-secret-to-getting-through-to-absolutely-anyone-computer-icons-books-icon-circle-iconset-martz90-love-photography-heart.png'/>
+      </header>
       <Menu />
       <Description />
+      <nav>
+        <div>
+          hdjfdlkj
+        </div>
+      </nav>
     </div>
   );
 }
