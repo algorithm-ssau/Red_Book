e8d997b06416a2b90ded22cfb20674072b1d9723
kosmosmars <kosmosmarsm@yandex.ru>
2022-04-06 13:07:18 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import './App.css';
 import Description from './Components/Description';
 import Menu from './Components/Menu';
-import Mammals from './Animals/Mammals';
+import Mammals from './Animals/mammals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar, Nav, FormControl, Container,Button} from 'react-bootstrap';
 
