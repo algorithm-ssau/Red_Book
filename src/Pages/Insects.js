e13afd282fc894816eff4733c1d3046d6ba2bc6c
lilloberry <99738029+lilloberry@users.noreply.github.com>
2022-04-17 14:14:33 +0400

    Update Insects.js

--- src/Pages/Insects.js
+++ src/Pages/Insects.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import Insect1 from '../Animals/Lentochnica.jpg'
+import Insect2 from '../Animals/Anopli.jpg'
 
 export default class Insects extends Component {
   render() {
     return (
-      <div>
-        insects
+      <div className="contacts">
+
+      <div className="contact-card">
+          <img align="center" src={Insect1}/>
+          <h3><font color="#3AC1EF"> Ленточница розовобрюхая </font></h3>
+          <p>Бабочки отмечаются в светлых лиственных лесах 
+            и лесных колках в лесостепи. Гусеница в мае-июне
+            питается листьями ивы козьей.</p>
+      </div>
+      <div className="contact-card">
+          <img align="center" src={Insect2}/>
+          <h3><font color="#3AC1EF"> Аноплий самарский </font></h3>
+          <p>Самый крупный представитель семейства Pompilidae
+            в Самарской области. Предпочитает открытые стации 
+            с легкими песчаными почвами.</p>
       </div>
     )
   }
