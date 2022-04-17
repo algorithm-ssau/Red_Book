b888efdf7b6d5872ddee08e319dfabb4d9150037
lilloberry <99738029+lilloberry@users.noreply.github.com>
2022-04-17 13:16:24 +0400

    Update Fish.js

--- src/Pages/Fish.js
+++ src/Pages/Fish.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import Fish1 from '../Animals/Fish1.jpg'
 
 export default class Fish extends Component {
   render() {
     return (
-      <div>
-        Fish
+      <div className="contacts">
+
+      <div className="contact-card">
+          <img align="center" src={Fish1}/>
+          <h3><font color="#3AC1EF"> Форель ручьёвая </font></h3>
+          <p>Форель — жилая рыба в ручьях со сравнительно чистой водой 
+            и каменистым дном. Питается всякого рода мелкими животными, 
+            крупные особи охотно поедают падающих в воду насекомых.</p>
       </div>
+      
+      
+  </div>
     )
   }
 }
