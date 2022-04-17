6bc570c84b0ef84ce6a82c141483a45b7de9afeb
Marya Malyugina <loveamu@mail.ru>
2022-04-17 14:36:21 +0400

    Insects update

--- src/Pages/Insects.js
+++ src/Pages/Insects.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Insect1 from '../Animals/Lentochnica.jpg'
 import Insect2 from '../Animals/Anopli.jpg'
+import LuHan from '../Animals/LuHan.jpg'
+import ButterFly from '../Animals/Butterfly.jpg'
 
 export default class Insects extends Component {
   render() {
@@ -20,6 +22,18 @@ export default class Insects extends Component {
           <p>Самый крупный представитель семейства Pompilidae
             в Самарской области. Предпочитает открытые стации 
             с легкими песчаными почвами.</p>
+      </div>
+      <div className="contact-card">
+          <img align="center" src={LuHan}/>
+          <h3><font color="#3AC1EF"> Жук-олень </font></h3>
+          <p>Приурочен к старым дубравам и широколиственным лесам 
+            при наличии в них дуба. Самый крупный жук Европы.</p>
+      </div>
+      <div className="contact-card">
+          <img align="center" src={ButterFly}/>
+          <h3><font color="#3AC1EF"> Голубянка альцет </font></h3>
+          <p>Лёт в регионе отмечен в июне–июле. В год развивается одна 
+            генерация. Численность и тенденции ее изменения неизвестны.</p>
       </div>
       </div>
     )
