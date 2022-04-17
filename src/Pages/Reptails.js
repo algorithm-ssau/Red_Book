c099c4cbb9189e8de25e7dfb60a8a690224ba3cc
Marya Malyugina <loveamu@mail.ru>
2022-04-17 13:06:14 +0400

    Frog

--- src/Pages/Reptails.js
+++ src/Pages/Reptails.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
-import img1 from '../Animals/1.png'
-import img2 from '../Animals/2.png'
-import img3 from '../Animals/3.png'
-import img4 from '../Animals/4.png'
+import Frog from '../Animals/Frog.jpg'
+
 import './reptailsView.css';
 
 
@@ -12,7 +10,7 @@ export default class Reptails extends Component {
       <div className="contacts">
 
       <div className="contact-card">
-          <img align="center" src={img1}/>
+          <img align="center" src={Frog}/>
           <h3><font color="#3AC1EF"> Лягушка травяная </font></h3>
           <p>Травяная лягушка обитает на заболоченных лугах 
             и во влажных лесах около выходов грунтовых вод, 
