1ded66068e16be78e98c7108cd0ce1577ee7dd4b
lilloberry <99738029+lilloberry@users.noreply.github.com>
2022-05-01 15:46:42 +0400

    Update Insects.js

--- src/Pages/Insects.js
+++ src/Pages/Insects.js
@@ -3,6 +3,8 @@ import Insect1 from '../Animals/Lentochnica.jpg'
 import Insect2 from '../Animals/Anopli.jpg'
 import LuHan from '../Animals/LuHan.jpg'
 import ButterFly from '../Animals/Butterfly.jpg'
+import Insect3 from '../Animals/Slizen.jpg'
+import Insect4 from '../Animals/Kallistus.jpg'
 
 export default class Insects extends Component {
   render() {
@@ -34,6 +36,20 @@ export default class Insects extends Component {
           <h3><font color="#3AC1EF"> Голубянка альцет </font></h3>
           <p>Лёт в регионе отмечен в июне–июле. В год развивается одна 
             генерация. Численность и тенденции ее изменения неизвестны.</p>
+      </div>
+      <div className="contact-card">
+          <img align="center" src={Insect3}/>
+          <h3><font color="#3AC1EF"> Слизень сизо-чёрный </font></h3>
+          <p>Слизни обитают в оврагах, низинах и на участках, на которых 
+          при более низких и стабильных летних температурах поддерживается 
+          высокая влажность. Днём скрывается под корой валежника, в гниющей древесине.</p>
+      </div>
+      <div className="contact-card">
+          <img align="center" src={Insect4}/>
+          <h3><font color="#3AC1EF"> Каллистус лунный </font></h3>
+          <p>Гигрофильный вид, встречается по берегам водоемов с поверхностным покровом, 
+            сформировавшимся на карбонатных породах или на размытых известняках. 
+            Встречен и вдали от водоема у родника, истекающего из известняков.</p>
       </div>
       </div>
     )
