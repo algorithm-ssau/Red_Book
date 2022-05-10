14b1e64d4e96797e96f83e327c557be31bb3bb6f
kosmosmars <kosmosmarsm@yandex.ru>
2022-05-10 11:53:31 +0400

    test

--- models/post.js
+++ models/post.js
@@ -1,10 +1,19 @@
 const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
 
+mongoose.connect("mongodb+srv://user_1:GxD7rkKIUyZPvHUE@cluster0.fzwse.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true });
 
 const postSchema = new Schema({
         name: String,
         description: String
 });
 
-const Post = mongoose.model ('Fish', postSchema)
+const Fish = mongoose.model ('Fish', postSchema)
+
+Fish.find({}, function(err, docs){
+        mongoose.disconnect();
+         
+        if(err) return console.log(err);
+         
+        console.log(docs);
+    }); 
