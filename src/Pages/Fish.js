e52cd843f60f6c121779191c914d8e3e2f8ec62a
kosmosmars <kosmosmarsm@yandex.ru>
2022-05-19 21:18:36 +0400

    БД_Fish

--- src/Pages/Fish.js
+++ src/Pages/Fish.js
@@ -8,76 +8,100 @@ import Fish6 from '../Animals/Whitefishibicha.jpg'
 import Fish7 from '../Animals/Fastandfurios.jpg'
 import Fish8 from '../Animals/Understone.jpg'
 
-export default class Fish extends Component {
+let check = false;
+let list1;
+export default class MyComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/api/items")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+
+      )
+      console.log(this.state.items);
+  }
+
   render() {
     GetUsers();
-    return (
-      <div className="contacts">
+    if (check)
+    {
+      console.log(list1[0]['description'])
+      let b = 5;
+    return(
+    <div className="contacts">
 
-      <div className="contact-card">
-          <img align="center" src={Fish1}/>
-          <h3><font color="#3AC1EF"> Форель ручьёвая </font></h3>
-          <p>Форель — жилая рыба в ручьях со сравнительно чистой водой 
-            и каменистым дном. Питается всякого рода мелкими животными, 
-            крупные особи охотно поедают падающих в воду насекомых.</p>
-      </div>
-      <div className="contact-card">
-          <img align="center" src={Fish2}/>
-          <h3><font color="#3AC1EF"> Осётр русский </font></h3>
-          <p> Обитает в русловой части водохранилищ. Взрослые питаются 
-            преимущественно рыбой, второстепенное значение имеют моллюски, 
-            ракообразные, личинки насекомых, черви.</p>
-      </div>
-      <div className="contact-card">
-          <img align="center" src={Fish3}/>
-          <h3><font color="#3AC1EF"> Белуга </font></h3>
-          <p> Проходной вид, обитает в  Каспийском море, в  реки заходит для
-            размножения. Взрослая белуга питается в основном рыбой.
-            Плодовитость до 7,5 млн икринок. С</p>
-      </div>
-      <div className="contact-card">
-          <img align="center" src={Fish4}/>
-          <h3><font color="#3AC1EF"> Шип </font></h3>
-          <p> Длина до  2  м, масса до  30  кг. Живет не  менее 30  лет. 
-            Ранее обычный промысловый вид, численность которого была подорвана 
-            до создания каскада водохранилищ.</p>
-      </div>
-      <div className="contact-card">
-          <img align="center" src={Fish5}/>
-          <h3><font color="#3AC1EF"> Стерлядь </font></h3>
-          <p> Живет в реках, водохранилищах, реже в озерах. 
-            Предпочитает проточные условия, биотопы с песчаным и галечным грунтом. 
-            Плодовитость более 100 тыс. икринок.</p>
-      </div>
-      <div className="contact-card">
-          <img align="center" src={Fish6}/>
-          <h3><font color="#3AC1EF"> Белорыбица </font></h3>
-          <p> Крупная хищная рыба, достигающая свыше 1 м длины и до 20 кг веса. 
-            Размножение не чаще 3 раз в жизни с интервалами в 2 года. 
-            Плодовитость до 390000 икринок.</p>
-      </div>
-      <div className="contact-card">
-          <img align="center" src={Fish7}/>
-          <h3><font color="#3AC1EF"> Быстрянка </font></h3>
-          <p> В озерах и прудах встречается крайне редко. Держится постоянно 
-            на поверхности и по быстроте движений превосходит уклейку. 
-            Икру откладывает на мелководьях, предпочитает каменистые грунты.</p>
-      </div>
-      <div className="contact-card">
-          <img align="center" src={Fish8}/>
-          <h3><font color="#3AC1EF"> Подкаменщик обыкновенный </font></h3>
-          <p> В дневное время малоактивен и прячется под камнями или в зарослях. 
-            Питается донными беспозвоночными и икрой рыб. Икру рыбы откладывают
-            в специально вырытые ямки или на нижнюю сторону камней.</p>
-      </div>
-      
-      
-
-
-  </div>
+    <div className="contact-card">
+        <img align="center" src={Fish1}/>
+        <h3><font color="#3AC1EF"> Форель ручьёвая </font></h3>
+        <p>{list1[0]['description']}</p>
+    </div>
+    
+    <div className="contact-card">
+        <img align="center" src={Fish2}/>
+        <h3><font color="#3AC1EF"> Осётр русский </font></h3>
+        <p> {list1[1]['description']}</p>
+    </div>
+    <div className="contact-card">
+        <img align="center" src={Fish3}/>
+        <h3><font color="#3AC1EF"> Белуга </font></h3>
+        <p> {list1[2]['description']}</p>
+    </div>
+    <div className="contact-card">
+        <img align="center" src={Fish4}/>
+        <h3><font color="#3AC1EF"> Шип </font></h3>
+        <p> {list1[3]['description']}</p>
+    </div>
+    <div className="contact-card">
+        <img align="center" src={Fish5}/>
+        <h3><font color="#3AC1EF"> Стерлядь </font></h3>
+        <p> Живет в реках, водохранилищах, реже в озерах. 
+          Предпочитает проточные условия, биотопы с песчаным и галечным грунтом. 
+          Плодовитость более 100 тыс. икринок.</p>
+    </div>
+    <div className="contact-card">
+        <img align="center" src={Fish6}/>
+        <h3><font color="#3AC1EF"> Белорыбица </font></h3>
+        <p> Крупная хищная рыба, достигающая свыше 1 м длины и до 20 кг веса. 
+          Размножение не чаще 3 раз в жизни с интервалами в 2 года. 
+          Плодовитость до 390000 икринок.</p>
+    </div>
+    <div className="contact-card">
+        <img align="center" src={Fish7}/>
+        <h3><font color="#3AC1EF"> Быстрянка </font></h3>
+        <p> В озерах и прудах встречается крайне редко. Держится постоянно 
+          на поверхности и по быстроте движений превосходит уклейку. 
+          Икру откладывает на мелководьях, предпочитает каменистые грунты.</p>
+    </div>
+    <div className="contact-card">
+        <img align="center" src={Fish8}/>
+        <h3><font color="#3AC1EF"> Подкаменщик обыкновенный </font></h3>
+        <p> В дневное время малоактивен и прячется под камнями или в зарослях. 
+          Питается донными беспозвоночными и икрой рыб. Икру рыбы откладывают
+          в специально вырытые ямки или на нижнюю сторону камней.</p>
+    </div>
+</div>
     )
   }
 }
+
+}
+
+
 
 async function GetUsers() {
   // отправляет запрос и получаем ответ
@@ -85,11 +109,17 @@ async function GetUsers() {
       method: "GET",
       headers: { "Accept": "application/json" }
   });
+
   // если запрос прошел нормально
-  if (response.ok === true) {
       // получаем данные
-      const users = await response.json();
-      console.log(users);
-  }
-  else {console.log('Errors');}
+  const users = await response.json();
+  check = true;
+      //users = users[0].name;
+  //console.log(users);
+  //console.log(users);
+  list1 = users;
+  return users;
 }
+
+
+
